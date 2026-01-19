c83d6d35348329d5c7361c60170825fbaa7fe12a
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2026-01-20 00:14:00 +0530

    Shorten the URL

---
+++ url-shortner-sb/src/main/java/com/url/shortner/controller/UrlMappingController.java
@@ -0,0 +1,39 @@
+package com.url.shortner.controller;
+
+import com.url.shortner.dtos.UrlMappingDTO;
+import com.url.shortner.models.User;
+import com.url.shortner.service.UrlMappingService;
+import com.url.shortner.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/urls")
+@AllArgsConstructor
+public class UrlMappingController {
+    private UrlMappingService urlMappingService;
+    private UserService userService;
+
+    // {"originalUrl":"https://example.com"}
+//    https://abc.com/QN7XOa0a --> https://example.com
+
+    @PostMapping("/shorten")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<UrlMappingDTO> createShortUrl(@RequestBody Map<String, String> request,
+                                                        Principal principal){
+        String originalUrl = request.get("originalUrl");
+        User user = userService.findByUsername(principal.getName());
+        UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
+        return ResponseEntity.ok(urlMappingDTO);
+    }
+
+}
---
+++ url-shortner-sb/src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -0,0 +1,64 @@
+package com.url.shortner.service;
+
+import com.url.shortner.dtos.UrlMappingDTO;
+import com.url.shortner.models.UrlMapping;
+import com.url.shortner.models.User;
+import com.url.shortner.repository.UrlMappingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UrlMappingService {
+
+    private UrlMappingRepository urlMappingRepository;
+
+    public UrlMappingDTO createShortUrl(String originalUrl, User user) {
+        String shortUrl = generateShortUrl();
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setShortUrl(shortUrl);
+        urlMapping.setUser(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+        UrlMapping savedUrlMapping = urlMappingRepository.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+
+    private UrlMappingDTO convertToDto(UrlMapping urlMapping){
+        UrlMappingDTO urlMappingDTO = new UrlMappingDTO();
+        urlMappingDTO.setId(urlMapping.getId());
+        urlMappingDTO.setOriginalUrl(urlMapping.getOriginalUrl());
+        urlMappingDTO.setShortUrl(urlMapping.getShortUrl());
+        urlMappingDTO.setClickCount(urlMapping.getClickCount());
+        urlMappingDTO.setCreatedDate(urlMapping.getCreatedDate());
+        urlMappingDTO.setUsername(urlMapping.getUser().getUsername());
+        return urlMappingDTO;
+    }
+
+    private String generateShortUrl() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+
+        for (int i = 0; i < 8; i++) {
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return shortUrl.toString();
+    }
+
+    public List<UrlMappingDTO> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+
+
+}
